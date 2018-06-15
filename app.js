9e75d269ae3253c915d20986000f90a431564507
Soheila Bayat <soheila.bayat1990@gmail.com>
2018-06-15 11:47:33 -0400

    soheila edition

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname + 'public'))
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+
+
+const PORT = 1337;
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`)
+});
